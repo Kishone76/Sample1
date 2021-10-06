dbf768cc762a38869889079863b353174177ef93
Kishone76 <kishone76@gmail.com>
2021-10-06 11:20:31 +0530

    done

---
+++ src/org/test/Employee.java
@@ -0,0 +1,25 @@
+package org.test;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Employee {
+	public static void main(String[] args) {
+		List<Integer>l=new LinkedList<Integer>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		l.add(50);
+		l.add(60);
+		
+		l.add(2,50);
+		System.out.println(l);
+			
+		}
+		
+	
+	}
+
+
